7c1d8c733b7989c4704dd81e5178101cef05924e
0natj <natalie@Natty.local>
2015-05-16 16:43:36 +1200

    yay

--- app.js
+++ app.js
@@ -1,53 +1,123 @@
-$("#login").show();
-$("#app").hide();
-// Authenticate with Dropbox
-var client = new Dropbox.Client({key: "pt4od8cpggu10ql"});
-client.authenticate({interactive: false}, function (error) {
-    if (error) {
-        alert('Authentication error: ' + error);
-    }
+//We're using the express framework and the mailgun-js wrapper
+var express = require('express');
+var Mailgun = require('mailgun-js');
+//init express
+var app = express();
+
+
+//Your api key, from Mailgun’s Control Panel
+var api_key = 'key-0c526ada06020fdf5a520a0de9dc7db4';
+
+//Your domain, from the Mailgun Control Panel
+var domain = 'krat.nz';
+
+//Your sending email address
+var from_who = 'johnstonenatalie@gmail.com';
+
+//Tell express to fetch files from the /js directory
+app.use(express.static(__dirname + '/js'));
+//We're using the Jade templating language because it's fast and neat
+app.set('view engine', 'jade')
+
+//Do something when you're landing on the first page
+app.get('/', function(req, res) {
+    //render the index.jade file - input forms for humans
+    res.render('index', function(err, html) {
+        if (err) {
+            // log any error to the console for debug
+            console.log(err); 
+        }
+        else { 
+            //no error, so send the html to the browser
+            res.send(html)
+        };
+    });
 });
-if (client.isAuthenticated()) {
-    $("#login").hide();
-    $("#app").show();
-}
-
-// Bind authenticate method to your login button and listen for click on button
-$("#login").on("click", client.authenticate());
 
 
-var datastoreManager = client.getDatastoreManager();
-datastoreManager.openDefaiultDatastore(function (error, datastore) {
-    if (error) {
-        alert('Error opening default datastore: ' + error);
+// Send a message to the specified email address when you navigate to /submit/someaddr@email.com
+// The index redirects here
+app.get('/submit/:mail', function(req,res) {
+
+    //We pass the api_key and domain to the wrapper, or it won't be able to identify + send emails
+    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+    var data = {
+    //Specify email data
+      from: from_who,
+    //The email to contact
+      to: req.params.mail,
+    //Subject and text data  
+      subject: 'Hello from Mailgun',
+      html: 'Hello, This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS! <a href="http://0.0.0.0:3030/validate?' + req.params.mail + '">Click here to add your email address to a mailing list</a>'
     }
 
-    // Let the user read all tasks by printing them to the screen
-    var taskTable = datastore.getTable('tasks');
-    var results = taskTable.query({completed: false});
-
-    for (var k=0; k<results.length;k++ ) {
-        $("#todos").append( "<li>"+results[k].get("taskname") + "</li>");
-    }
-    $("li").addClass("list-group-item");
-
-
-    // Let users add tasks
-    $("#add").on("click", function() {
-        taskTable.insert({
-            taskname: $("#newTask").val(),
-            completed: false,
-            created: new Date()
-        });
-
+    //Invokes the method to send emails given the above data with the helper library
+    mailgun.messages().send(data, function (err, body) {
+        //If there is an error, render the error page
+        if (err) {
+            res.render('error', { error : err});
+            console.log("got an error: ", err);
+        }
+        //Else we can greet    and leave
+        else {
+            //Here "submitted.jade" is the view file for this landing page 
+            //We pass the variable "email" from the url parameter in an object rendered by Jade
+            res.render('submitted', { email : req.params.mail });
+            console.log(body);
+        }
     });
 
-    // As new tasks are added automatically update the task list
-    datastore.recordsChanged.addListener(function (event) {
-        var records = event.affectedRecordsForTable('tasks');
-        for (var k=0; k<records.length;k++ ) {
-            $("#todos").append( "<li>"+records[k].get("taskname") + "</li>");
-        }
-        $("li").addClass("list-group-item");    
-    });     
 });
+
+app.get('/validate/:mail', function(req,res) {
+    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+    var members = [
+      {
+        address: req.params.mail
+      }
+    ];
+//For the sake of this tutorial you need to create a mailing list on Mailgun.com/cp/lists and put its address below
+    mailgun.lists('NAME@MAILINGLIST.COM').members().add({ members: members, subscribed: true }, function (err, body) {
+      console.log(body);
+      if (err) {
+            res.send("Error - check console");
+      }
+      else {
+        res.send("Added to mailing list");
+      }
+    });
+
+})
+
+app.get('/invoice/:mail', function(req,res){
+    //Which file to send? I made an empty invoice.txt file in the root directory
+    //We required the path module here..to find the full path to attach the file!
+    var path = require("path");
+    var fp = path.join(__dirname, 'invoice.txt');
+    //Settings
+    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+
+    var data = {
+      from: from_who,
+      to: req.params.mail,
+      subject: 'An invoice from your friendly hackers',
+      text: 'A fake invoice should be attached, it is just an empty text file after all',
+      attachment: fp
+    };
+
+
+    //Sending the email with attachment
+    mailgun.messages().send(data, function (error, body) {
+        if (error) {
+            res.render('error', {error: error});
+        }
+            else {
+            res.send("Attachment is on its way");
+            console.log("attachment sent", fp);
+            }
+        });
+})
+
+app.listen(3030);
--- nickmaria.js
+++
@@ -1,2 +0,0 @@
-    var Mailgun = require('mailgun');
-    Mailgun.initialize('sandboxcc4d08c8eea140c59dbe0af831c79ca0.mailgun.org', 'key-0c526ada06020fdf5a520a0de9dc7db4');
