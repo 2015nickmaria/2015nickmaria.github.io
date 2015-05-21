769bcb0354c7aa251773b5b0e1dfcc1177cd94c8
NAT <natalie@end-game.com>
2015-05-21 14:21:52 +1200

    updates
    heeey

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.use(express.static(__dirname + '/js'));
 
 app.use("/styles", express.static(__dirname + '/styles'));
 
+var bodyParser = require('body-parser')
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended: true
+})); 
+
 var Mailgun = require('mailgun-js');
 
 //Your api key, from Mailgun’s Control Panel
@@ -38,26 +44,24 @@ app.get('/', function(req, res) {
         };
     });
 });
-
-
-// Send a message to the specified email address when you navigate to /submit/someaddr@email.com
-// The index redirects here
-app.get('/submit/:mail', function(req,res) {
-
-    //We pass the api_key and domain to the wrapper, or it won't be able to identify + send emails
+app.post('/submit', function(req, res) {
+    
+    var name = req.body.name,
+        music = req.body.music,
+        attending = req.body.attending,
+        comments = req.body.comments;
+    
+    //console.log("req.body", name, music, yeah, nah, comments)
+    
     var mailgun = new Mailgun({apiKey: api_key, domain: domain});
-
+    
     var data = {
-    //Specify email data
       from: from_who,
-    //The email to contact
-      to: to_who,//req.params.mail,
-    //Subject and text data  
+      to: to_who,
       subject: 'Wedding RSVP',
-      html: '<h3>Hey Nick and Maria</h3> Name: ' + req.params.name + ' <br /> Song Request: ' + req.params.music + ' <br /> Comments: ' + req.params.comments +' <br /> Yeah: ' + req.params.yeah + ' <br /> Nah: ' + req.params.nah +  'This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS!'
+      html: '<h3>Hey Nick and Maria</h3> Name: ' + name + ' <br /> Attending: ' + attending + ' <br /> Song Request: ' + music + ' <br /> Comments: ' + comments + '<br/>'
     }
-
-    //Invokes the method to send emails given the above data with the helper library
+    
     mailgun.messages().send(data, function (err, body) {
         //If there is an error, render the error page
         if (err) {
@@ -68,64 +72,98 @@ app.get('/submit/:mail', function(req,res) {
         else {
             //Here "submitted.jade" is the view file for this landing page 
             //We pass the variable "email" from the url parameter in an object rendered by Jade
-            res.render('submitted', { email : req.params.mail });
+            res.render('submitted', { email : to_who });
             console.log(body);
         }
     });
-
 });
 
-app.get('/validate/:mail', function(req,res) {
-    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+// Send a message to the specified email address when you navigate to /submit/someaddr@email.com
+//// The index redirects here
+//app.get('/submit', function(req,res) {
+//console.log(req.body )
+//    //We pass the api_key and domain to the wrapper, or it won't be able to identify + send emails
+//    //var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+//    console.log("raaaaaa", req);
+//    var data = {
+//    //Specify email data
+//      from: from_who,
+//    //The email to contact
+//      to: to_who,//req.params.mail,
+//    //Subject and text data  
+//      subject: 'Wedding RSVP',
+//      html: '<h3>Hey Nick and Maria</h3> Name: ' + req.params.name + ' <br /> Song Request: ' + req.params.music + ' <br /> Comments: ' + req.params.comments +' <br /> Yeah: ' + req.params.yeah + ' <br /> Nah: ' + req.params.nah +  'This is not a plain-text email, I wanted to test some spicy Mailgun sauce in NodeJS!'
+//    }
 
-    var members = [
-      {
-        address: req.params.mail
-      }
-    ];
-//For the sake of this tutorial you need to create a mailing list on Mailgun.com/cp/lists and put its address below
-    mailgun.lists('NAME@MAILINGLIST.COM').members().add({ members: members, subscribed: true }, function (err, body) {
-      console.log(body);
-      if (err) {
-            res.send("Error - check console");
-      }
-      else {
-        res.send("Added to mailing list");
-      }
-    });
+    //Invokes the method to send emails given the above data with the helper library
+//    mailgun.messages().send(data, function (err, body) {
+//        //If there is an error, render the error page
+//        if (err) {
+//            res.render('error', { error : err});
+//            console.log("got an error: ", err);
+//        }
+//        //Else we can greet    and leave
+//        else {
+//            //Here "submitted.jade" is the view file for this landing page 
+//            //We pass the variable "email" from the url parameter in an object rendered by Jade
+//            res.render('submitted', { email : req.params.mail });
+//            console.log(body);
+//        }
+//    });
 
-})
+//});
 
-app.get('/invoice/:mail', function(req,res){
-    //Which file to send? I made an empty invoice.txt file in the root directory
-    //We required the path module here..to find the full path to attach the file!
-    var path = require("path");
-    var fp = path.join(__dirname, 'invoice.txt');
-    //Settings
-    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
-
-    var data = {
-      from: from_who,
-      to: req.params.mail,
-      subject: 'An invoice from your friendly hackers',
-      text: 'A fake invoice should be attached, it is just an empty text file after all',
-      attachment: fp
-    };
-
-
-    //Sending the email with attachment
-    mailgun.messages().send(data, function (error, body) {
-        if (error) {
-            res.render('error', {error: error});
-        }
-            else {
-            res.send("Attachment is on its way");
-            console.log("attachment sent", fp);
-            }
-        });
-})
+//app.get('/validate/:mail', function(req,res) {
+//    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+//
+//    var members = [
+//      {
+//        address: req.params.mail
+//      }
+//    ];
+////For the sake of this tutorial you need to create a mailing list on Mailgun.com/cp/lists and put its address below
+//    mailgun.lists('NAME@MAILINGLIST.COM').members().add({ members: members, subscribed: true }, function (err, body) {
+//      console.log(body);
+//      if (err) {
+//            res.send("Error - check console");
+//      }
+//      else {
+//        res.send("Added to mailing list");
+//      }
+//    });
+//
+//})
+//
+//app.get('/invoice/:mail', function(req,res){
+//    //Which file to send? I made an empty invoice.txt file in the root directory
+//    //We required the path module here..to find the full path to attach the file!
+//    var path = require("path");
+//    var fp = path.join(__dirname, 'invoice.txt');
+//    //Settings
+//    var mailgun = new Mailgun({apiKey: api_key, domain: domain});
+//
+//    var data = {
+//      from: from_who,
+//      to: req.params.mail,
+//      subject: 'An invoice from your friendly hackers',
+//      text: 'A fake invoice should be attached, it is just an empty text file after all',
+//      attachment: fp
+//    };
+//
+//
+//    //Sending the email with attachment
+//    mailgun.messages().send(data, function (error, body) {
+//        if (error) {
+//            res.render('error', {error: error});
+//        }
+//            else {
+//            res.send("Attachment is on its way");
+//            console.log("attachment sent", fp);
+//            }
+//        });
+//})
 
 //app.listen(3030);
 app.listen(process.env.PORT || 3030, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
--- js/main.js
+++ js/main.js
@@ -1,15 +1,15 @@
-var vForm = document.getElementById('mgform');
-var vInput = document.getElementById('mail');
-
-vForm.onsubmit = function() {
-//if (this.hid == "bulk") {
-    location = "/submit/" + encodeURIComponent('johnstonenatalie@gmail.com');
+//var vForm = document.getElementById('mgform');
+//var vInput = document.getElementById('mail');
+//
+//vForm.onsubmit = function() {
+////if (this.hid == "bulk") {
+//    location = "/submit/" + encodeURIComponent('johnstonenatalie@gmail.com');
+////}
+////if (this.hid == "list") {
+////    location = "/validate/" + encodeURIComponent(vInput.value);
+////}
+////if (this.hid == "inv") {
+////    location = "/invoice/" + encodeURIComponent(vInput.value);
+////}
+//    return false;
 //}
-//if (this.hid == "list") {
-//    location = "/validate/" + encodeURIComponent(vInput.value);
-//}
-//if (this.hid == "inv") {
-//    location = "/invoice/" + encodeURIComponent(vInput.value);
-//}
-    return false;
-}
